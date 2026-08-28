116a03fc7081b8c26883e107938b1a4138f63fe6
Atique Bin Zakir <atiqueb.zakir29@gmail.com>
2026-08-29 00:30:54 +0600

    do not invoke dmenu when theres only one shadow, bring directly to foreground

--- shadowRealm/shadowRealmSummon.sh
+++ shadowRealm/shadowRealmSummon.sh
@@ -1,8 +1,15 @@
 bufferPath="/home/phaetan/atiqxe/scripts/shadowRealm/shadows.txt"
+shadowCount=$(cat $bufferPath | wc -l)
 
-choice=$(dmenu -i -nb '#1d2021' -nf '#ebdbb2' -sb '#ebdbb2' -sf '#1d2021' -fn 'JetBrainsMono:pixelsize=16' -p " Summon ▨ >" -l 5 < "$bufferPath")
-[[ -z "$choice" ]] && exit 0
+if [[ $shadowCount == 1 ]]; then
+    winId=$(cat $bufferPath | cut -d "♮" -f 2)
+    xdo show "$winId" && echo -n "" > "$bufferPath"
+else
+    choice=$(dmenu -i -nb '#1d2021' -nf '#ebdbb2' -sb '#ebdbb2' -sf '#1d2021' -fn 'JetBrainsMono:pixelsize=16' -p " Summon ▨ >" -l 5 < "$bufferPath")
+    [[ -z "$choice" ]] && exit 0
 
-winId=$(echo "$choice" | cut -d "♮" -f 2)
-xdo show "$winId" && grep -Fxv "$choice" "$bufferPath" > tmp 
-mv tmp "$bufferPath"
+    winId=$(echo "$choice" | cut -d "♮" -f 2)
+    xdo show "$winId" && grep -Fxv "$choice" "$bufferPath" > tmp 
+    mv tmp "$bufferPath"
+fi 
+
